6acf6c34efabd321874d58c1bdd4adc944365398
TS60S <TS60S@DESKTOP-IOQ63TB>
2024-10-15 13:04:40 +0900

    초기 커밋.

---
+++ Form1.cs
@@ -0,0 +1,12 @@
+using Peak.Can.Basic;
+
+namespace PCanMiniView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
